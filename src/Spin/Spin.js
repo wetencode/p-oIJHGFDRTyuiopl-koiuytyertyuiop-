91709e8218e8d14b1e4a358e9f5a11f993297c36
firsttraining <t9511015@gmail.com>
2024-05-22 23:42:26 -0400

    empty

---
+++ src/Spin/Spin.js
@@ -0,0 +1,21 @@
+import './spin.css';
+
+function FastSpin(){
+    return(
+        <>
+
+
+        <div>
+            <div className='spinner'>
+
+            </div>
+            <p className='sam_abs' style={{color:'green', fontSize:'15px', wordSpacing:'1px'}}>Please wait...</p>
+
+        </div>
+
+
+        </>
+    )
+};
+
+export default FastSpin;
